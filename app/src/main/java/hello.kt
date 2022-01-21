1307354256c41ed244dcc5fd36274b73a8023644
sun <niketjssun31@gmail.com>
2022-01-21 15:33:32 +0900

    코틀린 기본 문법 끝

--- app/src/main/java/hello.kt
+++ app/src/main/java/hello.kt
@@ -11,6 +11,38 @@ fun main(){
     println(person.age)
     println(person)
 
+    val eagle = Eagle()
+    eagle.move()
+    eagle.eat()
+
+    //늦은 초기화:lateinit(var)
+    lateinit var k: String
+    k="Hello"
+    println(k)
+
+    //늦은 초기화:lazy(val)
+    val str:String by lazy{
+        println("초기화")
+        "hello"
+    }
+    println(str) //초기화, hello
+    println(str) //hello
+
+    //null값이 아님을 보증(!!)
+    val name:String?="키다리"
+    //val name2:String=name -에러
+    val name3:String?=name
+    val name4:String=name!!
+
+    //안전한 호출(?.) null값이 아닐때만 호출, null이면 null반환
+    val sc:String?=null
+    var upperCase=if(sc!=null) sc else null //null
+    upperCase=sc?.toUpperCase()//null
+
+    //엘비스 연산자?:안전한 호출시 null이아닌 기본값을 반환 할 때 사용
+    val str2:String?=null
+    var upperCase2=if(str2!=null)str2 else null
+    upperCase2=str2?.toUpperCase()?:"초기화 하시오"//null이면 문구 반환
 
 }
 
@@ -68,6 +100,56 @@ class B:A(){
 val a=B()   //OK
 
 //인터페이스
+interface Runnable{
+    fun run()
+    fun fastRun()=println("빨리 달린다.") //자바의 default처럼 구현된 메서드를 포함할수있다.
+
+
+}
+class Human:Runnable{
+    override fun run(){
+        println("달린다.")
+    }
+}
+//상속과 인터페이스를 함께 구현하기
+
+open class Beast{
+
+}
+interface movable{
+    fun move()
+    fun fastmove()=println("빨리 달리다")
+}
+interface Eatable{
+    fun eat()
+}
+class Eagle:Beast(),movable,Eatable{
+    override fun eat(){
+        println("야미")
+    }
+    override fun move(){
+        println("달리자")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
