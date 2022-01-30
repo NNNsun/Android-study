919f12be77618d887180744d7f6f873da423a984
sun <niketjssun31@gmail.com>
2022-01-30 22:08:07 +0900

    reCommit

--- app/src/main/java/com/chobo/xylophone/MainActivity.kt
+++ app/src/main/java/com/chobo/xylophone/MainActivity.kt
@@ -2,10 +2,16 @@ package com.chobo.xylophone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chobo.xylophone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
